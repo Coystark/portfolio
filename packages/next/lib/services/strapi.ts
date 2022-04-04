df434b26ca9d169daf25fab978bc2e0096950604
coystark <chcp.caio@gmail.com>
2022-04-03 21:14:26 -0300

    revert strapi baseurl

--- packages/next/lib/services/strapi.ts
+++ packages/next/lib/services/strapi.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
 export const strapi = Axios.create({
-  baseURL: 'https://portfolio-strapi-95.herokuapp.com/api',
+  baseURL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
   timeout: 30 * 1000,
 });
